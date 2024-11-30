83e29925ebfd545c8b2b346250477e7983c3e37b
Danil Gudebskiy <42720221+SalamiSlasher@users.noreply.github.com>
2024-11-30 21:54:57 +0500

    commit

--- card_methods.py
+++ card_methods.py
@@ -8,6 +8,7 @@ class Suit(Enum):
     DIAMONDS = "diamonds"
     CLUBS = "clubs"
     SPADES = "spades"
+    VOID = 'void'
 
 class Rank(IntEnum):
     SIX = 6
@@ -19,6 +20,7 @@ class Rank(IntEnum):
     QUEEN = 12
     KING = 13
     ACE = 14
+    VOID = 15
 
 class Card:
     def __init__(self, suit: Suit, rank: Rank) -> None:
@@ -29,6 +31,12 @@ class Card:
         return f"{self.rank.name} of {self.suit.name}"
 
 
+def can_beat(attack_card: Card, defend_card: Card, trump: Suit) -> bool:
+    if attack_card.suit == defend_card.suit:
+        return defend_card.rank > attack_card.rank
+    return defend_card.suit == trump
+
+
 def get_full_deck() -> list[Card]:
     deck = [Card(suit, rank) for suit, rank in itertools.product(Suit, Rank)]
     shuffle(deck)
@@ -37,3 +45,18 @@ def get_full_deck() -> list[Card]:
 
 def get_possible_movements(state: list[Card]) -> list[Card]:
     return []
+
+
+def possible_attack_cards(card_stack: list[Card], player_cards: list[Card]) -> list[Card]:
+    # Собираем ранги всех карт на столе, исключая пустые карты
+    table_ranks = set(card.rank for card in card_stack if not is_void_card(card))
+    # Возвращаем карты из руки игрока, ранги которых есть на столе
+    possible_cards = [card for card in player_cards if card.rank in table_ranks]
+    return possible_cards
+
+
+void_card = Card(Suit.VOID, Rank.VOID)
+
+
+def is_void_card(card: Card):
+    return void_card == card
--- game_env.py
+++ game_env.py
@@ -4,7 +4,7 @@ import random
 from gymnasium import Env, spaces
 from collections import deque
 
-from card_methods import Card
+from card_methods import Card, Suit, void_card
 import card_methods
 
 class DurakEnv(Env):
@@ -15,9 +15,10 @@ class DurakGame:
     def __init__(self):
         # self.player_count = player_count
         #self.players = deque(Player() for i in range(player_count))
-        self.trump = None
+        self.trump: Suit | None = None
         self.players = deque(maxlen=6)
         self.deck: list[Card] = card_methods.get_full_deck()
+        self.beat: list[Card] = []
 
     def add_player(self, player: Player):
         self.players.append(player)
@@ -31,7 +32,7 @@ class DurakGame:
             for player in self.players:
                 player.get_cards(self.deck)
 
-        self.trump = self.deck[0]
+        self.trump: Suit = self.deck[0].suit
         # --------------INIT--------------
 
         while self.end_condition():
@@ -39,34 +40,48 @@ class DurakGame:
             defender = self.players[1]
             co_attacker = None if len(self.players) == 2 else self.players[-1]
 
-            self.attack_loop(attacker, defender, co_attacker)
+            turn_stack = []
+            is_beaten_off = self.attack_loop(attacker, defender, co_attacker, turn_stack)
 
-    def attack_loop(self, attacker, defender, co_attacker, turn_stack=[], is_beaten_off=False):
+    def attack_loop(self, attacker, defender, co_attacker, turn_stack: list[CardLoopStackItem], is_beaten_off=True):
+        if len(defender) - len(turn_stack) == 0:
+            return is_beaten_off
+
         if not attacker.want_to_attack(turn_stack):
             if co_attacker and co_attacker.want_to_attack(turn_stack):
                 return self.attack_loop(co_attacker, defender, attacker, turn_stack)
-            return
+            return is_beaten_off
 
         # last card in turn_stack is card that must be beated by defender
-        attacker.attack(turn_stack)
-        is_beaten_off = defender.defend(turn_stack) # отбил ли? True <-> да
+        attack_card = attacker.attack(turn_stack)
 
+        # Когда дефендер отбил на прошлом ходу
+        if is_beaten_off:
+            defend_card = defender.defend(turn_stack)
+        # Когда дефендер не отбил в прошлом ходе и его топят...
+        else:
+            defend_card = void_card
+
+        turn_stack_item = CardLoopStackItem(attack_card, attacker, defend_card, defender)
+        turn_stack.append(turn_stack_item)
+
+        is_beaten_off = defend_card != void_card  # void_card - не смог отбить
         self.attack_loop(attacker, defender, co_attacker, turn_stack, is_beaten_off)
-
-            if is_beaten_off:
-                self.attack_loop(attacker, defender, co_attacker, turn_stack, is_beaten_off)
-            else:
-                pass
-
-
-
-
 
     def end_condition(self):
         return self.players == 1
 
     def give_card(self, player: Player):
         player.get_card(self.deck.pop())
+
+
+class CardLoopStackItem:
+    def __init__(self, attacker_card: Card, attacker: Player, defender_card: Card, defender: Player):
+        self.attacker_card = attacker_card
+        self.attacker = attacker
+
+        self.defender_card: Card = defender_card
+        self.defender = defender
 
 
 class Player:
@@ -84,10 +99,13 @@ class Player:
         if len(state) == 0:
             return True
 
-    def defend(self, state) -> bool:
+    def __len__(self):
+        return len(self.cards)
+
+    def defend(self, state) -> Card | None:
         # True <-> player beat an attack card
         to_defend = state[-1]
-        return False
+        return random.choice(self.cards)
 
 if __name__ == '__main__':
     game = DurakGame()
